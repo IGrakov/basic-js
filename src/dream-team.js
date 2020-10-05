002e73b2ee9c46d68dd32c3b6837649cfaba885c
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-05 18:51:12 +0300

    feat: add dream-team algorithm

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members.isArray()) {
+    return false;
+  }
+  res = [];
+
+  members.array.forEach(element => {
+    if (typeof element === 'string' ) 
+      if (element.length > 0) {
+        element.trim();
+        res.push(element[0]);
+    }
+  });
+
+  return res.sort().join('').toUpperCase();
 };
