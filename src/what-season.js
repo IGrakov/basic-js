e166d1f322464859cf266b1b352e0041bf0f8390
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-06 12:32:45 +0300

    fix: correct checing for valid argument type

--- src/what-season.js
+++ src/what-season.js
@@ -6,11 +6,12 @@ module.exports = function getSeason(inputDate) {
 
   if (typeof inputDate === 'undefined') {
     return 'Unable to determine the time of year!';
-  } else if (!(inputDate instanceof Date)) {
+  // else if (!(inputDate && Object.prototype.toString.call(inputDate) === "[object Date]" && !isNaN(inputDate)))
+  } else if (!(Object.prototype.toString.call(inputDate) === "[object Date]")) {
     throw new Error ('Argument is invalid');
   }
 
-  let inputMonth = inputDate.getMonth;
+  let inputMonth = inputDate.getMonth();
 
   if (inputMonth === 11 || inputMonth === 0 || inputMonth === 1) {
     return seasons[0];
