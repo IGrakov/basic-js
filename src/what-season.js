f3081a3879da2219273911be7298d712d83753b5
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-06 11:50:46 +0300

    feat: add what-season algorithm

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(inputDate) {
+
+  const seasons = [winter, spring, summer, autumn];
+
+  if (typeof inputDate === 'undefined') {
+    return 'Unable to determine the time of year!';
+  } else if (!(inputDate instanceof Date)) {
+    throw new Error ('Argument is invalid');
+  }
+
+  let inputMonth = inputDate.getMonth;
+
+  if (inputMonth === 12 || inputMonth === 1 || inputMonth === 2) {
+    return seasons[0];
+  } else if (inputMonth > 2 && inputMonth < 6) {
+    return seasons[1];
+  } else if (inputMonth > 5 && inputMonth < 9) {
+    return seasons[2];
+  } else {
+    return seasons[3];
+  }
+
+}
