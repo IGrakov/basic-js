e70c68e5d7e12da9b95addbf69260fa7c57dcbc0
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-06 19:18:30 +0300

    refactor: add comments

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -6,7 +6,8 @@ module.exports = function calculateHanoi(disksNumber = 0, turnsSpeed = 0) {
     turns: 0,
     seconds: 0
   }
-  
+  // if not arguments or zero input do nothing
+  // number of turns to solve Tower of Hanoi is 2^n - 1, where is number of disks
   if (disksNumber > 0 && turnsSpeed > 0) {
     result.turns = 2 ** disksNumber - 1;
     result.seconds = Math.floor(result.turns * 3600 / turnsSpeed);
