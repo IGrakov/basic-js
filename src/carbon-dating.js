f552a92264e0e3b1cadfdf175607791e3232fa75
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-06 10:54:29 +0300

    fix: add handling inadequate input values

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,14 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-const INITIAL_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const INITIAL_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity = 0) {
     if (typeof sampleActivity != 'string') {
         return false;
     }
     let presentActivity = parseInt(sampleActivity);
-    if (isNaN(presentActivity)) {
+    if (isNaN(presentActivity) || presentActivity <= 0 || presentActivity > INITIAL_ACTIVITY) {
         return false;
     }
     let calculatedTime = Math.ceil(Math.abs((HALF_LIFE_PERIOD / Math.log(2)) * Math.log(presentActivity / INITIAL_ACTIVITY)));
