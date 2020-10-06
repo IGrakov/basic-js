53db65e22f72003184a7a1c6f04b09117e7fa660
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-06 18:57:12 +0300

    feat: add hanoi-tower algorithm

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi(disksNumber = 0, turnsSpeed = 0) {
+
+  let result = {
+    turns: 0,
+    seconds: 0
+  }
+
+  function calculateHanoiInner(disksNumber = 0, turnsSpeed = 0) {
+    if (disksNumber === 1) {
+      result.turns++;
+      result.seconds += 3600 / turnsSpeed;
+    } else {
+      calculateHanoiInner(disksNumber - 1, turnsSpeed);
+      result.turns++;
+      result.seconds += 3600 / turnsSpeed;
+      calculateHanoiInner(disksNumber - 1, turnsSpeed);
+    }
+  }
+  
+  if (disksNumber > 0 && turnsSpeed > 0) {
+    calculateHanoiInner(disksNumber, turnsSpeed);
+  }
+
+  result.seconds = Math.floor(result.seconds);
+
+  return result;
+}
