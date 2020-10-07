8c784549f499ee22c1ef59e38bb3b35aab22b580
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-07 11:52:45 +0300

    refactor: update algorithm

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,18 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-/*   constructor() {
-    this.depthMax = 0;
-  } */
 
   calculateDepth(arr) {
-    let maxDepth = 0;
-    if (Array.isArray(arr)) {
-      arr.forEach(element => {
-        let currentDepth = calculateDepth(element);
-        currentDepth > maxDepth ? maxDepth = currentDepth : 0;
-      });
-    }
-    return maxDepth + 1;
+    return Array.isArray(arr) ? 1 + arr.reduce(function(a, b) {
+      // get the largest by comparing all the adjacent elements dimension
+      return Math.max(a, calculateDepth(b));
+      }, 0) : 0;
   }
 }
