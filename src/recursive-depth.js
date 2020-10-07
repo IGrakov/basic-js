ee2eb178b2f93b048f87c9d6cb28923ebebbf836
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-07 12:22:05 +0300

    fix: fix some errors

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,8 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  constructor() {
-    this.calculateDepth();
-  }
 
-  calculateDepth(arr) {
+  calculateDepth(arr = []) {
     return Array.isArray(arr) ? 1 + arr.reduce(function(a, b) {
       // get the largest by comparing all the adjacent elements dimension
       return Math.max(a, calculateDepth(b));
