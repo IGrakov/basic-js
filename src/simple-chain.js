3be529b87e4d4b687abb2532886fb1518faffed1
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-29 21:31:25 +0300

    feat: add initial encryption algorithm

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  let chain = '';
+
   getLength() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,83 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    let cipheringMachineType = type;
+    let dict = ['A', 'B', 'C', 'D', 'E',
+                'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O',
+                'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y', 'Z'];
+  }
+
+  encrypt(message, key) {
+    if (message === null || key === null) throw Error;
+    let messageToEncrypt = message.toUpperCase();
+    let encryptionKey = key.toUpperCase();
+    let encryptedMessage = '';
+
+    let plainMessageToEncrypt = '';
+    let plainEncryptionKey = '';
+    let fullEncryptionKey = '';
+
+    // select only Latin letters from the original message
+    // i.e. plain message
+    for (let i = 0; i < messageToEncrypt.length; i++) {
+      if (dict.indexOf(messageToEncrypt[i]) !== -1) {
+        plainMessageToEncrypt += messageToEncrypt[i];
+      }
+    }
+
+    // calculate repeated key length based on plain message
+    let factor = Math.trunc(plainMessageToEncrypt.length / encryptionKey.length);
+    let remainder = plainMessageToEncrypt.length % encryptionKey.length;
+    plainEncryptionKey = encryptionKey.repeat(factor) + encryptionKey.substring(0, remainder);
+
+    // make full key by adding arbitrary symbol at the same positions
+    // where other symbols than Latin letters are in original message
+    let counter = 0;
+    for (let i = 0; i < messageToEncrypt.length; i++) {
+      if (dict.indexOf(messageToEncrypt[i]) !== -1) {
+        fullEncryptionKey += plainEncryptionKey[counter];
+        counter++;
+      } else {
+        fullEncryptionKey += "x";
+      }
+    }
+
+    for (let i = 0; i < messageToEncrypt.length; i++) {
+      // if symbol in message to encrypt is not a Latin letter
+      // meaning it is not in the dict add it to resulting message unchanged
+      if (dict.indexOf(messageToEncrypt[i]) === -1) {
+        encryptedMessage += messageToEncrypt[i];
+      } else {
+        encryptedMessage += dict[(dict.indexOf(messageToEncrypt[i]) + 
+                                        dict.indexOf(fullEncryptionKey[i])) % 26];
+      }
+    }
+  
+    if (cipheringMachineType) {
+      return encryptedMessage
+    } else {
+      return encryptedMessage.split('').reverse.join('');
+    }
+  }
+
+  decrypt(message, key) {
+    let messageToDecrypt = message.toUpperCase();
+    let decryptionKey = key.toUpperCase();
+    let decryptedMessage = '';
+  
+    let factor = Math.trunc(messageToDecrypt.length / decryptionKey.length);
+    let remainder = messageToDecrypt.length % decryptionKey.length;
+    let decryptionKey = decryptionKey.repeat(factor) + decryptionKey.substring(0, remainder);
+
+    if (cipheringMachineType) {
+      return decryptedMessage
+    } else {
+      decryptedMessage.split('').reverse.join('');
+    }
   }
 }
 
