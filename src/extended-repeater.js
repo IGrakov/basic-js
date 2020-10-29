d366e7f1a40729b1ea32fc58dee1e963b7e446bf
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-29 15:11:33 +0300

    feat: add repeating algorithm

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -9,21 +9,59 @@ module.exports = function repeater(str, options) {
   let additionSeparator = '|';
   let resultingStr = '';
 
-  if (typeof str != 'string') {
-    initialString = str.toString();
-  } else {
-    initialStr = str;
-  }
+  // if (typeof str != 'string') {
+  //   initialString = str.toString();
+  // } else {
+  //   initialStr = str;
+  // }
 
-  if (typeof options.addition != 'string') {
-    addition = options.addition.toString();
-  } else {
-    initialStr = options.addition;
-  }
+  // if (typeof options.addition != 'string') {
+  //   addition = options.addition.toString();
+  // } else {
+  //   initialStr = options.addition;
+  // }
 
   if (options.repeatTimes === null || options.additionRepeatTimes === null) {
     return initialStr;
   }
+
+  if (options.repeatTimes != null) {
+    repeatTimes = options.repeatTimes;
+  }
+
+  if (options.additionRepeatTimes != null) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+
+  if (options.separator != null) {
+    separator = options.separator;
+  }
+
+  if (options.addition != null) {
+    addition = options.addition;
+  }
+
+  if (options.additionSeparator != null) {
+    additionSeparator = options.additionSeparator;
+  }
+
+  for (let i = 0; i < repeatTimes; i++) {
+    resultingStr += initialStr;
+
+    for (let j = 0; j < additionRepeatTimes; j ++) {
+      resultingStr += addition;
+
+      if (j < additionRepeatTimes - 1) {
+        resultingStr += additionSeparator;
+      }
+    }
+
+    if (i < repeatTimes - 1) {
+      resultingStr += separator;
+    }
+  }
+
+  return resultingStr;
 
 };
   
