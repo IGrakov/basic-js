a22f5c5758dc254e2592e0b05640144bb1c78f34
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-29 14:46:15 +0300

    feat: add inintial version of extended-repreater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let initialStr = '';
+  let repeatTimes = 0;
+  let separator = '+';
+  let addition = '';
+  let additionRepeatTimes = 0;
+  let additionSeparator = '|';
+  let resultingStr = '';
+
+  if (typeof str != 'string') {
+    initialString = str.toString();
+  } else {
+    initialStr = str;
+  }
+
+  if (typeof options.addition != 'string') {
+    addition = options.addition.toString();
+  } else {
+    initialStr = options.addition;
+  }
+
+  if (options.repeatTimes === null || options.additionRepeatTimes === null) {
+    return initialStr;
+  }
+
 };
   
