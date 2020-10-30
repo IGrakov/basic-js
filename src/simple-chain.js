650b2cca528c188d328c3e966118ea7f618412e5
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-30 17:28:50 +0300

    fix: correct reverse method

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -9,12 +9,17 @@ const chainMaker = {
     return this.chainValues.length;
   },
   addLink(value = '') {
-    this.chainValues.push(String(value));
-    if (value === '') {
-      this.chain.concat('~~( )');
+    if (value === '' && this.getLength() === 0) {
+      this.chain += '( )';
+    } else if (value === '') {
+    	this.chain += '~~( )';
+    } else if (this.getLength() === 0) {
+     	this.chain = this.chain + '( ' + String(value) + ' )';
     } else {
-      this.chain.concat('~~( ', String(value), ' )');
+      this.chain = this.chain + '~~( ' + String(value) + ' )';
     }
+
+    this.chainValues.push(String(value));
     return this;
   },
   removeLink(position) {
@@ -24,13 +29,28 @@ const chainMaker = {
 
     this.chain = '';
     for (let i = 0; i < this.chainValues.length; i++) {
-      this.chain.concat('~~( ', this.chainValues[i], ' )');
+    	if (this.chainValues[i] === '') {
+      	this.chain += '~~( )';
+      } else {
+      	this.chain = this.chain + '~~( ' + this.chainValues[i] + ' )';
+      }
     }
     return this;
   },
   reverseChain() {
-    this.chain = this.chain.split('').reverse().join('');
     this.chainValues = this.chainValues.reverse();
+    this.chain = '';
+    for (let i = 0; i < this.chainValues.length; i++) {
+    	if (this.chainValues[i] === '' && i === 0) {
+      	this.chain += '( )';
+      } else if(this.chainValues[i] === '') {
+      	his.chain += '~~( )';
+      } else if (i === 0) {
+        this.chain = this.chain + '( ' + this.chainValues[i] + ' )';
+      } else {
+      	this.chain = this.chain + '~~( ' + this.chainValues[i] + ' )';
+      }
+    }
     return this;
   },
   finishChain() {
