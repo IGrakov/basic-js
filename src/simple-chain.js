1f473f2c2afeee27b05b403b8a9ad8dbeb08d835
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-30 20:19:01 +0300

    fix: correct some errors

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,70 +2,27 @@
 
 const chainMaker = {
 
-  chain: '',
-  chainValues: [],
+  chain: [],
 
   getLength() {
-    return this.chainValues.length;
+    return this.chain.length;
   },
   addLink(value = '') {
-    let valueInit = String(value);
-    if (value === '' && this.getLength() === 0) {
-      this.chain += '( )';
-    } else if (value === '') {
-    	this.chain += '~~( )';
-    } else if (typeof value === 'object' && value !== null && this.getLength() === 0) {
-      this.chain += '( [object Object] )';
-      valueInit = '[object Object]';
-    } else if (typeof value === 'object' && value !== null ) {
-      this.chain += '~~( [object Object] )';
-      valueInit = '[object Object]';
-    } else if (this.getLength() === 0) {
-     	this.chain = this.chain + '( ' + String(value) + ' )';
-    } else {
-      this.chain = this.chain + '~~( ' + String(value) + ' )';
-    }
-
-    this.chainValues.push(valueInit);
+    value === undefined ? this.chain.push('( )') : this.chain.push(`( ${String(value)} )`);
     return this;
   },
   removeLink(position) {
-    if (Number.isNaN(position) || !Number.isInteger(position) || position > this.chainValues.length || this.chainValues < 1) throw Error;
-
-    this.chainValues.splice(position - 1 , 1);
-
-    this.chain = '';
-    for (let i = 0; i < this.chainValues.length; i++) {
-    	if (this.chainValues[i] === '' && i === 0) {
-      	this.chain += '( )';
-      } else if(this.chainValues[i] === '') {
-      	this.chain += '~~( )';
-      } else if (i === 0) {
-        this.chain = this.chain + '( ' + this.chainValues[i] + ' )';
-      } else {
-      	this.chain = this.chain + '~~( ' + this.chainValues[i] + ' )';
-      }
-    }
+    if (Number.isNaN(position) || !Number.isInteger(position) || position > this.chain.length || position < 1) throw Error;
+    this.chain.splice(position - 1 , 1);
     return this;
   },
   reverseChain() {
-    this.chainValues = this.chainValues.reverse();
-    this.chain = '';
-    for (let i = 0; i < this.chainValues.length; i++) {
-    	if (this.chainValues[i] === '' && i === 0) {
-      	this.chain += '( )';
-      } else if(this.chainValues[i] === '') {
-      	this.chain += '~~( )';
-      } else if (i === 0) {
-        this.chain = this.chain + '( ' + this.chainValues[i] + ' )';
-      } else {
-      	this.chain = this.chain + '~~( ' + this.chainValues[i] + ' )';
-      }
-    }
+    this.chain = this.chain.reverse();
     return this;
   },
   finishChain() {
-    return this.chain;
+
+    return this.chain.join('~~');
   }
 };
 
